b00cd7f90fb916dd7fb7bd12e2479576482cbba2
Tuba282 <tubajan282@gmail.com>
2026-07-08 10:49:48 +0500

    Firebase completed

--- Next JS/my-app/app/contact/page.tsx
+++ Next JS/my-app/app/contact/page.tsx
@@ -1,11 +1,38 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { db } from '@/src/utils/firebase';
 
 const ContactScreen = () => {
   const [users, setUsers] = useState([]);
+
+  const delUser = (userData: any) => {
+    console.log(userData);
+    deleteDoc(doc(db, "Users", userData.docId))
+      .then(() => {
+        console.log('User deleted successfully!');
+      })
+      .catch((err) => {
+        console.log('Err while deleting user:', err);
+      })
+      .finally(() => {
+        fetchUsersFromDB();
+      });
+  };
+
+  const updateUser = async (userData: any) => {
+    console.log(userData);
+
+    const docRef = doc(db, 'Users', userData.docId);
+    await updateDoc(
+      docRef,
+      {
+        email: "test@gmail.com",
+        password: 'test123'
+      }
+    );
+  };
 
   const fetchUsersFromDB = async () => {
     const getUsers = await getDocs(collection(db, 'Users'));
@@ -14,7 +41,7 @@ const ContactScreen = () => {
     let targetData: any = [];
 
     getUsers.forEach((eachUser) => {
-      console.log(eachUser.id, '--', eachUser.data());
+      // console.log(eachUser.id, '--', eachUser.data());
 
       const obj = {
         ...eachUser.data(),
@@ -36,7 +63,13 @@ const ContactScreen = () => {
       <ul>
         {
           users.map((item: any) => {
-            return <li key={item.uid}> {item.email} </li>
+            return (
+              <li key={item.uid} style={{ marginTop: '3%' }}>
+                {item.email.slice(0, item.email.indexOf('@'))}
+                <button onClick={() => delUser(item)}> Delete Item </button>
+                <button onClick={() => updateUser(item)}> Update Item </button>
+              </li>
+            )
           })
         }
       </ul>
