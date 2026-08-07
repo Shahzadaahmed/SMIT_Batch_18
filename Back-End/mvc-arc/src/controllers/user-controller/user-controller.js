2cb5f4785c94d3afb78d7ff84aa3d584fca2b5a5
Tuba282 <tubajan282@gmail.com>
2026-08-07 10:56:21 +0500

    Fetch and delete functionality implemented in mongo db

--- Back-End/mvc-arc/src/controllers/user-controller/user-controller.js
+++ Back-End/mvc-arc/src/controllers/user-controller/user-controller.js
@@ -1,6 +1,7 @@
 // All user related controller functions are defined here...!
 
 import UserModal from "../../modals/user-modal/user-modal.js";
+import mongoose from "mongoose";
 
 const greetUser = (req, res) => {
     return res.status(200).send({
@@ -8,10 +9,9 @@ const greetUser = (req, res) => {
     });
 };
 
-// Save / create ysaer controller...!
+// Save / create usaer controller...!
 const createUser = async (req, res) => {
     try {
-
         const isUserExist = await UserModal.findOne({ email: req.body.email });
         if (isUserExist) {
             return res.status(400).send({
@@ -40,4 +40,76 @@ const createUser = async (req, res) => {
     };
 }
 
-export { greetUser, createUser };
+// fetch users controller...!
+const fetchUsers = async (req, res) => {
+    try {
+        const { role } = req.query;
+        console.log('Query:', role);
+
+        const counts = await UserModal.countDocuments();
+        console.log('Counts:', counts);
+        if (counts == 0) {
+            return res.status(400).send({
+                status: false,
+                message: "No User found",
+                data: []
+            });
+        }
+
+        const query = (role) ? ({ role }) : ({});
+        // const fetchData = await UserModal.find(query).select('userName email');
+        const fetchData = await UserModal.find(query).select('-password');
+        return res.status(200).send({
+            status: true,
+            message: "Users",
+            data: fetchData
+        });
+    }
+
+    catch (error) {
+        console.log(`Err while fetching user: ${error}`);
+        return res.status(500).send({
+            status: false,
+            message: "Err while fetching user!"
+        });
+    };
+};
+
+// Delete user controller...!
+
+const handleDeleteUser = async (req, res) => {
+    try {
+        const { uid } = req.params;
+        console.log('Uid:', uid);
+
+        const checkUid = mongoose.isValidObjectId(uid);
+
+        if (!checkUid) {
+            return res.status(400).send({
+                status: false,
+                message: "Invalid Uid"
+            });
+        }
+
+        // const delUser = await UserModal.findByIdAndDelete(uid);
+
+        const delUser = await UserModal.deleteOne({ _id: uid });
+
+        if (delUser) {
+            return res.status(200).send({
+                status: true,
+                message: "User deleted!"
+            });
+        };
+    }
+
+    catch (error) {
+        console.log(`Err while deletingt user: ${error}`);
+        return res.status(500).send({
+            status: false,
+            message: "Err while deletingt user!"
+        });
+    }
+}
+
+export { greetUser, createUser, fetchUsers, handleDeleteUser };
--- Back-End/mvc-arc/src/routes/user-routes/user-routes.js
+++ Back-End/mvc-arc/src/routes/user-routes/user-routes.js
@@ -1,12 +1,16 @@
 // All user related routes are defined here...!
 
 import express from "express";
-import { greetUser, createUser } from "../../controllers/user-controller/user-controller.js";
+import { greetUser, createUser , fetchUsers , handleDeleteUser } from "../../controllers/user-controller/user-controller.js";
 
 const router = express.Router();
 
 router.route('/').get(greetUser);
 
 router.route('/user/save').post(createUser);
+
+router.route('/users/fetch').get(fetchUsers);
+
+router.route('/user/delete/:uid').delete(handleDeleteUser);
 
 export default router;
